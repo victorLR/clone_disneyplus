17c409a9a175f27c34010f8629bc7852bde05148
victorLR <victoraffonso1997@gmail.com>
2024-09-22 08:25:56 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,19 +1,52 @@
 document.addEventListener('DOMContentLoaded',function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
     
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
 
     for (let i= 0; i< buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
         const abaAlvo = botao.target.dataset.tabButton;
         const aba= document.querySelector(`[data-tab-id=${abaAlvo}]`);
-        escondeTodoasAbas();
+        escondeTodasAbas();
         aba.classList.add('shows__list--is-active');
         removeBotaoAtivo();
         botao.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    for (let i = 0; i< questions.length; i++) {
+        questions[i].addEventListener('click',abreOuFechaResposta);
+    }
 })
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -24,7 +57,7 @@ function removeBotaoAtivo(){
 }
 
 
-function escondeTodoasAbas() {
+function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
 
